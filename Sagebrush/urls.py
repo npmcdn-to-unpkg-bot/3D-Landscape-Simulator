3defad09034acd6b4cb94864ff905ab5877e15af
Taylor Mutch <taylormutch@gmail.com>
2016-07-25 15:24:08 -0700

    Added heightmap generation service to django project.

--- Sagebrush/urls.py
+++ Sagebrush/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from ST_Sim_Landscape_Simulator import views
+from Heightmaps.views import generate_heightmap
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,5 +18,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^run_st_sim$', views.run_st_sim, name='st_sim'),
-    url(r'', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+
+    # Heightmap generation service
+    url(r'^heightmap/(?P<nlat>\d+\.\d+)/(?P<slat>\d+\.\d+)/(?P<elon>\-\d+\.\d+)/(?P<wlon>\-\d+\.\d+)$',
+                    generate_heightmap, name='heightmap'),
 )
