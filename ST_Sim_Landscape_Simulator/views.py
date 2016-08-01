5b054e300ff8a6f9a4d5982b339dcd9d7bdfe1f2
Taylor Mutch <taylormutch@gmail.com>
2016-08-01 12:35:17 -0700

    Better use of subprocesses in Python. Eliminated odd conversions for obtaining result scenario id.

--- ST_Sim_Landscape_Simulator/views.py
+++ ST_Sim_Landscape_Simulator/views.py
@@ -2,6 +2,8 @@ import os
 import csv
 import json
 
+import subprocess32 as sub_proc # for handling return codes better than os.system
+
 from django.conf import settings
 from collections import OrderedDict
 
@@ -41,7 +43,8 @@ def index(request):
 
 #def run_st_sim(st_scenario,feature_id): # for csv initial conditions
 def run_st_sim(st_scenario,veg_slider_values_dict):
-    print st_scenario
+
+    # TODO - handle multiple users
     st_initial_conditions_file=static_files_dir + "/st_sim/initial_conditions/user_defined_temp.csv"
 
     # Only for user defined initial conditions. Write initial conditions slider values to csv.
@@ -60,48 +63,42 @@ def run_st_sim(st_scenario,veg_slider_values_dict):
     st_library_file="ST-Sim-Sample-V3-0-24.ssim"
     st_library=st_library_path+os.sep+st_library_file
 
-    print "\nRunning scenario sid:" + st_scenario
+    #print "\nRunning scenario sid:" + st_scenario
     
     # Run ST-Sim with initial conditions and user specified scenario.
     st_initial_conditions_command="--import --lib=" + st_library + \
         " --sheet=STSim_InitialConditionsNonSpatialDistribution table_name --file="  + st_initial_conditions_file +  " --sid=" + st_scenario
-   
-    print "\nInitial conditions command: "
-    print st_exe + " " + st_initial_conditions_command
-    os.system(st_exe + " " + st_initial_conditions_command)
-    st_run_model_command="--run --lib=" + st_library + " --sid="+st_scenario
-    print "\nModel run command: "
-    print st_exe + " " + st_run_model_command
-    #print 'Outputting model results'
-    st_model_output_sid=str(os.system(st_exe + " " + st_run_model_command))
-    
-    actual_st_model_output_sid = str(256 + int(st_model_output_sid) / 256) if os.name == 'posix' else st_model_output_sid
-    #print int(st_model_output_sid)
-    #print "\nResulting scenario id: " + st_model_output_sid
-    print "\nResulting scenario id: " + st_model_output_sid
-    print "\nActual Resulting scenario id: " + actual_st_model_output_sid
-    st_model_results_dir=static_files_dir + "/st_sim/model_results"
+    sub_proc.call(st_exe + " " + st_initial_conditions_command, shell=True)
 
-    st_model_output_file="stateclass-summary-" +str(actual_st_model_output_sid)+ ".csv"
+    # Run the model process
+    # use Popen since we want to handle the output on the live
+    st_run_model_command="--run --lib=" + st_library + " --sid="+st_scenario
+    test_model_run = sub_proc.Popen(st_exe + " " + st_run_model_command, shell=True, stdout=sub_proc.PIPE)
+    result = ""
+
+    # wait for the process to end
+    while True: # TODO - implement a timeout? This should be handled VERY carefully...
+        out = test_model_run.stdout.read(1)
+        result += out
+        if out == '' and test_model_run.poll() != None:
+            break;
+
+    st_model_output_sid = result.split(" ")[-1].strip()
+    st_model_results_dir=static_files_dir + "/st_sim/model_results"
+    st_model_output_file="stateclass-summary-" + st_model_output_sid + ".csv"
 
     # Generate a report (csv) from ST-Sim for the model run above
-    st_report_command=" --console=stsim --create-report --name=stateclass-summary --lib=" + st_library + " --file=" + st_model_results_dir + os.sep + st_model_output_file + " --sids=" + actual_st_model_output_sid
-    print "Executing report..."
-    os.system(st_exe + " " + st_report_command)
-    print "\nReport command: "
-    print st_exe + " " + st_report_command
+    st_report_command=" --console=stsim --create-report --name=stateclass-summary --lib=" \
+     + st_library + " --file=" + st_model_results_dir + os.sep + st_model_output_file \
+     + " --sids=" + st_model_output_sid
+    sub_proc.call(st_exe + " " + st_report_command, shell=True)
+
     # Get results out of ST-Sim csv report.
-
     results_dict={}
-    print "\nOutput file location: "
-    print st_model_results_dir + os.sep + st_model_output_file
-
-    # debug
-    #debug_path_list = [st_model_results_dir, os.sep + 'stateclass-summary-260.csv']
-    #debug_path = "".join(debug_path_list)
+    #print "\nOutput file location: "
+    #print st_model_results_dir + os.sep + st_model_output_file
 
     reader=csv.reader(open(st_model_results_dir + os.sep + st_model_output_file))
-    #reader=csv.reader(open(debug_path))
     reader.next()
 
     for row in reader:
@@ -114,8 +111,8 @@ def run_st_sim(st_scenario,veg_slider_values_dict):
     rounded_dict = {k:round(v,1) for k, v in results_dict.items()}
     sorted_dict=OrderedDict(sorted(rounded_dict.items(), key=lambda t: t[0]))
     results_json=json.dumps(sorted_dict)
-    print "\nResults: "
-    print results_json
+    #print "\nResults: "
+    #print results_json
 
     context={
         'results_json':results_json
